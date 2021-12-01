d59df5aaeb25656e91ca1c25f6152e5545c29945
jventura1738 <justinv9001@aol.com>
2021-12-01 17:37:06 -0500

    Add Day 01

---
+++ 2021/DAY01_SONARSWEEP/sonar_sweep_P1.py
@@ -0,0 +1,10 @@
+# Justin Ventura Advent of Code
+
+if __name__ == '__main__':
+    lines = [line.rstrip('\n') for line in open('sonar_sweep_data.txt', 'r')]
+    depths = list(map(int, lines))
+    larger_than_prev = 0
+    for i in range(1, len(depths)):
+        if depths[i] > depths[i-1]:
+            larger_than_prev += 1
+    print(larger_than_prev)
